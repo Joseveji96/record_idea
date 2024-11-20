2ab680fa9630cd1c5c75b190279c3848c1b02990
Joseveji96 <joseveji96@gmail.com>
2024-11-19 23:24:14 -0600

    animations on audiorecorder component

--- components/RecordingAnim.tsx
+++ components/RecordingAnim.tsx
@@ -4,7 +4,7 @@ import { View } from 'react-native'
 const RecordingAnim = () => {
     return (
         <View style={{ width: "100%", height: 35, backgroundColor: "rgba(170, 50, 48, 0.28)", borderRadius: 6, marginTop: 5 }}>
-
+            
         </View>
     )
 }
